95a750a77164b5e70b0f0736309bf5da56daca1a
Scott <sfrasier@gmail.com>
2018-01-20 16:37:41 -0600

    Getting started

--- scripts/analytics.py
+++ scripts/analytics.py
@@ -1,9 +1,55 @@
 
 #!/usr/bin/env python3
+import csv
+
+
+def open_csv(path):
+        # Open the CSV file and return a list of list. each nested list is for a
+        # puppy and weights.
+    with open(path) as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)  # Skip the headers
+        return list(reader)
+
+
+def one_day(puppy_list):
+    if len(puppy_list) < 3:
+        print('Not enough data for one day gains')
+        return None
+    return(round(float(puppy_list[-1]) - float(puppy_list[-2]), 2))
+
+
+def two_day(puppy_list):
+    if len(puppy_list) < 4:
+        print('Not enough data for one day gains')
+        return None
+    return(round(float(puppy_list[-1]) - float(puppy_list[-3]), 2))
+
+
+def three_day(puppy_list):
+    if len(puppy_list) < 5:
+        print('Not enough data for one day gains')
+        return None
+    return(round(float(puppy_list[-1]) - float(puppy_list[-4]), 2))
+
+
+def four_day(puppy_list):
+    if len(puppy_list) < 6:
+        print('Not enough data for one day gains')
+        return None
+    return(round(float(puppy_list[-1]) - float(puppy_list[-5]), 2))
+
+
+def five_day(puppy_list):
+    if len(puppy_list) < 7:
+        print('Not enough data for one day gains')
+        return None
+    return(round(float(puppy_list[-1]) - float(puppy_list[-6]), 2))
 
 
 def main():
-    print('OK')
+    pass
+
 
 if __name__ == '__main__':
     main()
--- scripts/tests/tests.py
+++ scripts/tests/tests.py
@@ -18,5 +18,58 @@ def teardown_module(module):
 
 def test_file_open():
     # Open file read to list.
-    puppy_cict = A.open_text_file('testing_data/test_data.txt')
-    
+    puppy_list = A.open_csv('testing_data/test_data.csv')
+    assert puppy_list[0] == ['Puppy 1', '14', '14.9', '15.2', '15.85', '17.55', '19.95', '20.5', '22.6', '24.05']
+    assert puppy_list[1] == ['Puppy 2', '17.8', '18.4', '19.1', '20', '22.45', '23.3', '24', '25.2', '26.4']
+    assert puppy_list[2] == ['Puppy 3', '16.7', '17.3', '18.25', '20.05', '21.9', '23.05', '24.85', '24.9', '28.95']
+    assert puppy_list[3] == ['Puppy 4', '14.8', '14.8', '15.9', '16.7', '19.25', '20.8', '22.45', '23.65', '26.05']
+    assert puppy_list[4] == ['Puppy 5', '15.8', '16.7', '18.15', '19.9', '21.4', '22.95', '24.93', '26.3', '30.25']
+    assert puppy_list[5] == ['Puppy 6', '15.7', '15.83', '17', '18.6', '19.1', '20.7', '22', '22.35', '23.6']
+    assert puppy_list[6] == ['Puppy 7', '12.7', '13.3', '14.05', '16', '17.05', '18.75', '21.05', '22', '24.25']
+    assert puppy_list[7] == ['Puppy 8', '15.7', '16.3', '17.4', '19.2', '19.8', '20.65', '22.6', '23.75', '25.85']
+    assert puppy_list[8] == ['Puppy 9', '15.9', '16.1', '18', '19.95', '21.25', '22.1', '22.1', '24.25', '26.4']
+
+
+def test_one_day():
+    short = ['Puppy 1', '14']
+    puppy1 = ['Puppy 1', '14', '14.9', '15.2', '15.85', '17.55', '19.95', '20.5', '22.6', '24.05']
+    puppy9 = ['Puppy 9', '15.9', '16.1', '18', '19.95', '21.25', '22.1', '22.1', '24.25', '26.4']
+    assert A.one_day(short) == None
+    assert A.one_day(puppy1) == 1.45
+    assert A.one_day(puppy9) == 2.15
+
+
+def test_two_day():
+    short = ['Puppy 1', '14', '14.9']
+    puppy1 = ['Puppy 1', '14', '14.9', '15.2', '15.85', '17.55', '19.95', '20.5', '22.6', '24.05']
+    puppy9 = ['Puppy 9', '15.9', '16.1', '18', '19.95', '21.25', '22.1', '22.1', '24.25', '26.4']
+    assert A.two_day(short) == None
+    assert A.two_day(puppy1) == 3.55
+    assert A.two_day(puppy9) == 4.3
+
+
+def test_three_day():
+    short = ['Puppy 1', '14', '14.9', '15.2']
+    puppy1 = ['Puppy 1', '14', '14.9', '15.2', '15.85', '17.55', '19.95', '20.5', '22.6', '24.05']
+    puppy9 = ['Puppy 9', '15.9', '16.1', '18', '19.95', '21.25', '22.1', '22.1', '24.25', '26.4']
+    assert A.three_day(short) == None
+    assert A.three_day(puppy1) == 4.1
+    assert A.three_day(puppy9) == 4.3
+
+
+def test_four_day():
+    short = ['Puppy 1', '14', '14.9', '15.2', '15.85']
+    puppy1 = ['Puppy 1', '14', '14.9', '15.2', '15.85', '17.55', '19.95', '20.5', '22.6', '24.05']
+    puppy9 = ['Puppy 9', '15.9', '16.1', '18', '19.95', '21.25', '22.1', '22.1', '24.25', '26.4']
+    assert A.four_day(short) == None
+    assert A.four_day(puppy1) == 6.5
+    assert A.four_day(puppy9) == 5.15
+
+
+def test_four_five():
+    short = ['Puppy 1', '14', '14.9', '15.2', '15.85', '17.55']
+    puppy1 = ['Puppy 1', '14', '14.9', '15.2', '15.85', '17.55', '19.95', '20.5', '22.6', '24.05']
+    puppy9 = ['Puppy 9', '15.9', '16.1', '18', '19.95', '21.25', '22.1', '22.1', '24.25', '26.4']
+    assert A.five_day(short) == None
+    assert A.five_day(puppy1) == 8.2
+    assert A.five_day(puppy9) == 6.45
